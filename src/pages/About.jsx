b564be1598e019f9bc17ee47118c1864396d9e44
autture06 <arya.utture@gmail.com>
2024-05-21 00:45:02 -0400

    src folder updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
 import {skills, experiences, socialLinks} from '../constants';
