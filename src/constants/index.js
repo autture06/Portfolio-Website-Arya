4826ef5c9dc0f89728dd8dd86efa0ba3a78360ae
autture06 <arya.utture@gmail.com>
2024-07-19 01:21:04 -0400

    index js file updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -239,7 +239,7 @@ export const experiences = [
         title: "Software Engineer Participant",
         company_name: "JPMorgan Chase & Co.",
         icon: jpmc,
-        iconBg: "brown",
+        iconBg: "#7E5439",
         date: "May 2023 - May 2023",
         points: [
             "Established Local Development Environment: Efficiently set up a comprehensive local development environment by downloading and configuring all necessary files, tools, and dependencies.",
